07db7d80e26f4d10d69f39829c2d7a92a58adc90
h <haavard_martin@hotmail.com>
2024-08-05 22:43:03 +0200

    Test 3

--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,20 @@
   <div>
     <center class="mt-4">
       <TextTitle title="Hello" />
-
-      <p class="mb-4">
-        And I (still) have a <strong>plant</strong>
-        <v-icon color="green" class="ml-0">mdi-flower</v-icon>
-      </p>
-
-      <MyHr class="my-12" />
     </center>
 
     <MyCols :pages="pages" :cols="12" :xl="3" :lg="3" :md="12" :xs="12" v-slot="{ page }">
       <center>
         <nuxt-link :to="page.to">
-          <v-btn text x-large color="blue darken-2" dark>{{
-            page.title
-          }}</v-btn>
+          <v-btn text x-large color="blue darken-2" dark>{{ page.title }}</v-btn>
         </nuxt-link>
 
         <p class="grey--text">{{ page.description }}</p>
       </center>
     </MyCols>
-
   </div>
 </template>
+
 
 <script>
 export default {
@@ -35,17 +26,7 @@ export default {
           title: "Read my notes",
           description: "They are not too messy",
           to: "/content",
-        },
-        {
-          title: "CV",
-          description: "And other fun stuff",
-          to: "/cv",
-        },
-        /* { */
-        /*   title: "Consulting", */
-        /*   description: "And what services I offer", */
-        /*   to: "/consulting", */
-        /* }, */
+        }
       ],
     };
   },
